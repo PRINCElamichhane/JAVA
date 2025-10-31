e379e726577b1f459e71889255f04c6c6218b5c9
PRINCE <164494583+PRINCElamichhane@users.noreply.github.com>
2025-10-31 09:34:46 +0000

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main (string []arg) {
+
+        string[] guests = {"Ana", "Olivia", "Òscar", "Maria", "Francio", "Pedro", "Miguel"};
+
+
+        for (int i = 0;  i< guests.length; i++) {
+            System.out.println ("the party guests are")
+        }
+    }
+}
