1f7ca0f1a9b7043b3a8faaffcd27a472756b4a3d
PRINCE <164494583+PRINCElamichhane@users.noreply.github.com>
2026-01-23 15:35:49 +0000

    Add files via upload

---
+++ Ex27.java
@@ -0,0 +1,13 @@
+public class Ex27 {
+    public static void main(String[] args) {
+        String age = "30";
+        int ageInt = Integer.parseInt(age);
+        float ageFloat = Float.parseFloat(age);
+        System.out.println("Idade como inteiro: " + ageInt);
+        System.out.println("Idade como float: " + ageFloat);
+
+
+
+
+    }
+}
